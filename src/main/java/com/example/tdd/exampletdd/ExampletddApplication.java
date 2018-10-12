fa0b0765670908fdfe9e1b4d2bf74dc5e9453bc3
Lucas Moraes <lucashsmoraes@gmail.com>
2018-10-12 14:26:28 -0300

    Incializando o projeto

---
+++ src/main/java/com/example/tdd/exampletdd/ExampletddApplication.java
@@ -0,0 +1,12 @@
+package com.example.tdd.exampletdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampletddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampletddApplication.class, args);
+    }
+}
